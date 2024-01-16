7e534082d85f56c546a265e84549f1c34191efb1
Dienne <minhdien678@gmail.com>
2024-01-16 18:17:47 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+
+chat_log = []
+
+while True:
+    user_message = input()
+    if user_message.lower() == "quit":
+        break
+    else:
+        chat_log.append({"role": "user", "content": user_message})
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=chat_log
+        )
+
+        assistant_response = response.choices[0].message.content
+        print("ChatGPT: ", assistant_response.strip("\n").strip())
+        chat_log.append({"role": "assistant", "content": assistant_response.strip("\n").strip()})
